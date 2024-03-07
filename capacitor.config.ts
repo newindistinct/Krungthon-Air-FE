2a55fcde69b17f7921e8e4e6076118273ee9a479
newindistinct <new.indistinct@gmail.com>
2024-03-07 10:09:57 +0700

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Krungthon-Air-FE',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
